4bfd7c5bee712f97c3c86e88b14722d80068b600
Oleksii Lopasov <olopa@softserveinc.com>
2018-11-19 12:06:47 +0200

    dataproc_spark_jars added

--- wordcount-job-template.py
+++ wordcount-job-template.py
@@ -65,7 +65,8 @@ with models.DAG(
     # master node.
     run_dataproc_spark = dataproc_operator.DataProcSparkOperator(
         task_id='run_dataproc_spark',
-        main_jar=WORDCOUNT_JAR,
+        main_class='org.apache.spark.examples.JavaWordCount',
+        dataproc_spark_jars=WORDCOUNT_JAR,
         cluster_name='macys-cluster-{{ ds_nodash }}',
         arguments=input_file)
 
